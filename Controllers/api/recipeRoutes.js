cbe69a9c4a2678e081d8f823ef19a5d8756ecbcb
RealGavinPrice <116835834+RealGavinPrice@users.noreply.github.com>
2023-02-22 04:19:09 -0500

    more randorecipe.js -- need to change how this plugs in

--- Controllers/api/recipeRoutes.js
+++ Controllers/api/recipeRoutes.js
@@ -17,4 +17,161 @@ router.get('/:id', function(req, res, next) {
 }
 });
 
+
+
+
+
+
+
+
+///
+///
+///
+///
+
+
+const router = require('express').Router();
+
+const { User, Post, Comment } = require('../../model');
+
+const sequelize = require('../../config/connection');
+
+const auth = require('../../util/auth');
+
+
+
+// GET posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        // config for returning post data
+        attributes: [
+            'id',
+            'post_text',
+            'title',
+            'created_at',
+        ],
+        order: [[ 'created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Favorite,
+                attributes: ['id', 'recipe_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    // AND then!
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// GET a recipe
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'recipe_text',
+            'title',
+            'created_at',
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Favorite,
+                attributes: ['id', 'recipe_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'This is an unidentified flying recipe!!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+//CREATE new recipe
+router.post('/', auth, (req, res) => {
+    Post.create({
+        title: req.body.title,
+        post_text: req.body.post_text,
+        user_id: req.session.user_id
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// UPDATE a recipe
+router.put('/:id', auth, (req, res) => {
+    Post.update(req.body,
+        {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'This recipe does not exist yet!'});
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+// DELETE a recipe
+router.delete('/:id', auth, (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'This recipe already does not exist!!'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
 module.exports = router;
