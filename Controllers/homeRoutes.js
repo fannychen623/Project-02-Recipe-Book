066548a79e91e94b29e00ead947007fb49ed53a0
fycn <fannychen623@gmail.com>
2023-02-27 18:26:29 -0500

    updated catalog page, pagination functions, fixed login/signup issues, render my-kitchen and my-favorites page

--- Controllers/homeRoutes.js
+++ Controllers/homeRoutes.js
@@ -3,44 +3,36 @@ const { User, Recipe, Favorite } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+  res.render('homepage', { 
+    logged_in: req.session.logged_in 
+  });
 });
 
 router.get('/catalog', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const recipeData = await Recipe.findAll({
-      attributes: {
-        include: [[sequelize.fn('COUNT', sequelize.col('recipe_id')), 'recipes_count']]
-      },
       include: [
         {
           model: User,
           attributes: ['username'],
         },
-        {
-          model: Favorite,
-          attributes: [
-            [sequelize.fn('COUNT', sequelize.col('recipe_id')), 'favorites_count'],
-          ],
-          group:["recipe_id"]
-        },
+        // {
+        //   model: Favorite,
+        //   attributes: [
+        //     [sequelize.fn('COUNT', sequelize.col('recipe_id')), 'favorites_count'],
+        //   ],
+        //   group:["recipe_id"]
+        // },
       ],
     });
 
     // Serialize data so the template can read it
     const recipes = recipeData.map((recipe) => recipe.get({ plain: true }));
-    const remainder = recipes.recipes_count % 2; 
-    if (remainder == 0) {
-      const pageCount = Math.floor(recipes.recipes_count/2);
-    } else {
-      const pageCount = Math.floor(recipes.recipes_count/2) + 1 ;
-    }
     
     // Pass serialized data and session flag into template
     res.render('catalog', { 
       recipes, 
-      pageCount,
       logged_in: req.session.logged_in 
     });
   } catch (err) {
@@ -58,7 +50,7 @@ router.get('/my-kitchen', withAuth, async (req, res) => {
 
     const user = userData.get({ plain: true });
 
-    res.render('dashboard', {
+    res.render('my-kitchen', {
       ...user,
       logged_in: true
     });
@@ -67,7 +59,24 @@ router.get('/my-kitchen', withAuth, async (req, res) => {
   }
 });
 
+router.get('/my-favorites', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Recipe }],
+    });
 
+    const user = userData.get({ plain: true });
+
+    res.render('my-favorites', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // manage login
 router.get('/login', (req, res) => {
--- public/js/catalog.js
+++ public/js/catalog.js
@@ -1,57 +1,91 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById("1").style.visibility= "visible" ;
-  document.getElementById("2").style.visibility= "visible" ;
+  document.getElementById("0").style.display= "block" ;
+  document.getElementById("1").style.display= "block" ;
+  const page1 = document.getElementById("0");
+  const page2 = document.getElementById("1");
+  page1.querySelector('.image').style.transform= "rotate(353deg)";
+  if (page2) {
+    page2.querySelector('.image').style.transform= "rotate(7deg)";
+  }
+  document.getElementById("last-page").innerHTML = Math.ceil(document.querySelectorAll('.recipe-page').length/2);
 }, false);
 
 function nextPage() {
-  const currentPage = document.getElementById("current-page").value
-  const lastPage = document.getElementById("last-page").value
+  let currentPage = Number(document.getElementById("current-page").innerHTML)
+  let lastPage = Number(document.getElementById("last-page").innerHTML)
 
   if (currentPage < lastPage) {
     currentPage++;
     document.getElementById("current-page").textContent = currentPage;
-    const catalog2 = currentPage * 2
+
+    const catalog2 = (currentPage * 2) - 1
     const catalog1 = catalog2 - 1
+    console.log((catalog1 + 4) % 4)
+    const page1 = document.getElementById(catalog1);
+    const page2 = document.getElementById(catalog2);
 
-    if (lastPage % currentPage == 0) {
-      document.querySelector('.recipe-page').style.visibility= "hidden";
-      document.getElementById("'" + catalog1 + "'").style.visibility= "visible";
-      document.getElementById("'" + catalog1 + "'").style.visibility= "visible";
+    document.querySelectorAll('.recipe-page').forEach(function(el) {
+      el.style.display = "none";
+    });
+    page1.style.display= "block";
+    if (page2) {
+      page2.style.display= "block";
+    } else {
+      const blankPage = document.createElement("div");
+      blankPage.className = "tile is-parent is-4 recipe-page"
+      document.getElementById("recipe-posts").appendChild(blankPage);
     }
-  };
 
-  if (4 % catalog1 == 0) {
-    document.querySelectorAll('#' + catalog1 + '  > .image').style.transform= "rotate(353deg)";
-    document.querySelectorAll('#' + catalog2 + '  > .image').style.transform= "rotate(7deg)";
-  } else if (4 % catalog1 == 1) {
-    document.querySelectorAll('#' + catalog1 + '  > .image').style.transform= "rotate(7deg)";
-    document.querySelectorAll('#' + catalog2 + '  > .image').style.transform= "rotate(353deg)";
+    if ((catalog1 + 4) % 4 == 0) {
+      page1.querySelector('.image').style.transform= "rotate(353deg)";
+      if (page2) {
+        page2.querySelector('.image').style.transform= "rotate(7deg)";
+      }
+    } else if ((catalog1 + 4) % 4 == 2) {
+      page1.querySelector('.image').style.transform= "rotate(7deg)";
+      if (page2) {
+        page2.querySelector('.image').style.transform= "rotate(353deg)";
+      }
+    };
   };
 }
 
 function previousPage() {
-  const currentPage = document.getElementById("current-page").value
-  const lastPage = document.getElementById("last-page").value
+  let currentPage = Number(document.getElementById("current-page").innerHTML)
 
   if (currentPage > 1) {
     currentPage--;
     document.getElementById("current-page").textContent = currentPage;
-    const catalog2 = currentPage * 2
+
+    const catalog2 = (currentPage * 2) - 1
     const catalog1 = catalog2 - 1
+    console.log((catalog1 + 4) % 4)
+    const page1 = document.getElementById(catalog1);
+    const page2 = document.getElementById(catalog2);
 
-    if (lastPage % currentPage == 0) {
-      document.querySelector('.recipe-page').style.visibility= "hidden";
-      document.getElementById("'" + catalog1 + "'").style.visibility= "visible";
-      document.getElementById("'" + catalog1 + "'").style.visibility= "visible";
+    document.querySelectorAll('.recipe-page').forEach(function(el) {
+      el.style.display = "none";
+    });
+    page1.style.display= "block";
+    if (page2) {
+      page2.style.display= "block";
+    } else {
+      const blankPage = document.createElement("div");
+      blankPage.className = "tile is-parent is-4 recipe-page"
+      document.getElementById("recipe-posts").appendChild(blankPage);
     }
-  };
 
-  if (4 % catalog1 == 0) {
-    document.querySelectorAll('#' + catalog1 + '  > .image').style.transform= "rotate(353deg)";
-    document.querySelectorAll('#' + catalog2 + '  > .image').style.transform= "rotate(7deg)";
-  } else if (4 % catalog1 == 1) {
-    document.querySelectorAll('#' + catalog1 + '  > .image').style.transform= "rotate(7deg)";
-    document.querySelectorAll('#' + catalog2 + '  > .image').style.transform= "rotate(353deg)";
+    if ((catalog1 + 4) % 4 == 0) {
+      page1.querySelector('.image').style.transform= "rotate(353deg)";
+      if (page2) {
+        page2.querySelector('.image').style.transform= "rotate(7deg)";
+      }
+    } else if ((catalog1 + 4) % 4 == 2) {
+      page1.querySelector('.image').style.transform= "rotate(7deg)";
+      if (page2) {
+        page2.querySelector('.image').style.transform= "rotate(353deg)";
+      }
+    };
   };
 }
 
